a884584b867769903c4e0b13e5ae7e6271403d15
unicondo <bruno@unicondo.app>
2024-11-03 18:08:09 -0300

    Banco de Dados
    Arquivo de apoio para criação de banco de dados inicial.

---
+++ rifa_compras.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `rifa_compras` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `numeros` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
